992f5931e16cf5ce003f3fe110499da6b511121c
shazim8480 <shazm_99@yahoo.com>
2022-04-27 13:42:38 +0600

    document picker working

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+
+import * as DocumentPicker from "expo-document-picker";
 
 export default function App() {
+  const documentUploadHandler = async () => {
+    let result = await DocumentPicker.getDocumentAsync({});
+    alert(result.uri);
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Select a Document to Upload</Text>
+      <Text style={{ marginBottom: 20 }}>Select a Document to Upload</Text>
       <StatusBar style="auto" />
+      <Button title="Upload Document" onPress={documentUploadHandler} />
     </View>
   );
 }
